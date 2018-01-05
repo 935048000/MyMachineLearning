d7a62cb949ccc0b92ee10b6b3e3ffca9adac634a
vast <935048000@qq.com>
2018-01-05 17:27:56 +0800

    图像搜索，数据存取文件

---
+++ image_search/flask-keras-cnn-image-retrievalmaster/extract_cnn_vgg16_keras.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Author: yongyuan.name
+
+import numpy as np
+from numpy import linalg as LA
+
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+
+
+'''
+使用vgg16模型提取特征
+输出归一化特征向量
+'''
+def extract_feat(img_path):
+    # weights: 'imagenet'
+    # pooling: 'max' or 'avg'
+    # input_shape: (width, height, 3), width and height should >= 48
+    
+    input_shape = (224, 224, 3)
+    model = VGG16(weights = 'imagenet', input_shape = (input_shape[0], input_shape[1], input_shape[2]), pooling = 'max', include_top = False)
+        
+    img = image.load_img(img_path, target_size=(input_shape[0], input_shape[1]))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    feat = model.predict(img)
+    norm_feat = feat[0]/LA.norm(feat[0])
+    return norm_feat
