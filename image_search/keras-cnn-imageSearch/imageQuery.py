10c9feb4a2835c4fd6a7b4ccaed733f46560bb51
vast <935048000@qq.com>
2018-01-20 15:17:30 +0800

    图像搜索，数据存取文件

--- image_search/keras-cnn-imageSearch/imageQuery.py
+++ image_search/keras-cnn-imageSearch/imageQuery.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import argparse
 
+
 # 命令行参数功能
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-query", required = True,
@@ -25,20 +26,21 @@ import argparse
 
 
 # 显示
-def showimage(queryImage,imlist,result):
+def showimage(queryImage, imlist, result):
     # 读取和显示查询图像
-    queryImg = mpimg.imread(queryImage)
-    plt.title("Query Image")
-    plt.imshow(queryImg)
-    plt.show()
+    queryImg = mpimg.imread (queryImage)
+    plt.title ("Query Image")
+    plt.imshow (queryImg)
+    plt.show ()
     # 显示检索结果
-    for i,im in enumerate(imlist):
-        image = mpimg.imread(result+"/"+im)
-        plt.title("top %d" %(i+1))
-        plt.imshow(image)
-        plt.show()
-
+    for i, im in enumerate (imlist):
+        image = mpimg.imread (result + "/" + im)
+        plt.title ("top %d" % (i + 1))
+        plt.imshow (image)
+        plt.show ()
+    
     return 0
+
 
 # 图片信息目录
 DirPath = "D:/datasets/testingset001/"
@@ -46,62 +48,59 @@ result = "./imagesets"
 Model = "./image200CNN.h5"
 
 # 读取索引图像的特征向量和相应的图像名称
-h5f = h5py.File(Model,'r')
+h5f = h5py.File (Model, 'r')
 feats = h5f['dataset_1'][:]
 imgNames = h5f['dataset_2'][:]
-h5f.close()
-
+h5f.close ()
 
 # queryImage = "./imagesets/19700102142532557.JPEG"
 # queryImage = "D:/datasets/testingset001/19700102141601727.JPEG"
 # queryImage = "D:/datasets/trainingset1/19700102130026909.JPEG"
 queryImage = "D:/datasets/002/19700102132424765.JPEG"
 
-print("原图为：",queryImage)
+print ("原图为：", queryImage)
 queryImageName = queryImage[:-5]
-queryImageName = queryImageName+".txt"
-with open(queryImageName,'r',encoding='utf-8') as f:
-    print("原图信息：",f.readline())
-
+queryImageName = queryImageName + ".txt"
+with open (queryImageName, 'r', encoding='utf-8') as f:
+    print ("原图信息：", f.readline ())
 
 # 提取查询图像的特征，计算 simlarity 评分和排序
-queryVec = extract_feat(queryImage)
-scores = np.dot(queryVec, feats.T) # 计算点积（内积）,计算图像得分
+queryVec = extract_feat (queryImage)
+scores = np.dot (queryVec, feats.T)  # 计算点积（内积）,计算图像得分
 # 矩阵乘法并把（纵列）向量当作n×1 矩阵，点积还可以写为：a·b=a^T*b。
 # 点积越大，说明向量夹角越小。点积等于1，则向量为同向，向量夹角0度。
-rank_ID = np.argsort(scores)[::-1] # 排序,倒序，大到小
-rank_score = scores[rank_ID] # 计算评分
+rank_ID = np.argsort (scores)[::-1]  # 排序,倒序，大到小
+rank_score = scores[rank_ID]  # 计算评分
 # print("scores",scores,type(scores))
 # print ("rank_ID",rank_ID,type(rank_ID))
 # print ("rank_score",rank_score,type(rank_score))
 
 
 # 检索显示的相似度最高的图像数目
-maxres = 3 # 显示三个
+maxres = 3  # 显示三个
 imList = []
 scoresList = []
 for i, index in enumerate (rank_ID[0:maxres]):
-    _temp = imgNames[index].decode()
+    _temp = imgNames[index].decode ()
     imList.append (_temp)
 
 for j in rank_score[0:maxres]:
-    _temp = float("%.2f" % (j*100))
-    scoresList.append(_temp)
+    _temp = float ("%.2f" % (j * 100))
+    scoresList.append (_temp)
 
-print ("最高的%d张图片为: " %maxres, imList)
+print ("最高的%d张图片为: " % maxres, imList)
 
 imgInfo = []
 for j in imList:
     j = j[:-5]
-    j = j+".txt"
-    fileName = DirPath+j
-    with open(fileName,'r',encoding='utf-8') as f:
-        imgInfo.append(f.readline().strip("\n"))
+    j = j + ".txt"
+    fileName = DirPath + j
+    with open (fileName, 'r', encoding='utf-8') as f:
+        imgInfo.append (f.readline ().strip ("\n"))
 
-print ("图片信息为: ",imgInfo )
+print ("图片信息为: ", imgInfo)
 
+print ("最高%d张图片的相似度评分：" % maxres, scoresList)
 
-print ("最高%d张图片的相似度评分："%maxres,scoresList)
- 
 
 # showimage(queryImage,imList,result)
--- image_search/keras-cnn-imageSearch/test01.py
+++ image_search/keras-cnn-imageSearch/test01.py
@@ -20,17 +20,17 @@
 # start = time.time()
 # print ("任务01 Used Time:%s s，Mem Used：%dM  %.2f%% " % ((time.time () - start), mem.used / 1024 / 1024, mem.percent))
 
-import sys, time
-import pyprind
-
-# from pyprind import ProgBar
-import pyprind
-import time
-bar = pyprind.ProgBar(30,monitor=True,title="job01",bar_char="-")
-for i in range(30):
-    time.sleep (0.5)
-    bar.update()
-print(bar)
+# import sys, time
+# import pyprind
+#
+# # from pyprind import ProgBar
+# import pyprind
+# import time
+# bar = pyprind.ProgBar(30,monitor=True,title="job01",bar_char="-")
+# for i in range(30):
+#     time.sleep (0.5)
+#     bar.update()
+# print(bar)
 
 
 # import pyprind
@@ -42,3 +42,7 @@ print(bar)
 # import time
 # for progress in pyprind.prog_percent (range (20)):
 #     time.sleep (1)
+
+
+queryImage = "D:/datasets/002/19700102132424765.JPEG"
+print(queryImage.split("/")[-1])
